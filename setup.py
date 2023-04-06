bf69f6c4148568a92c16d12e4e6b5393a7f13d15
yekta <ngyekta@gmail.com>
2023-04-06 06:03:40 +0300

    Remove dep version

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@ from setuptools import setup
 
 setup(
     name="kandinsky2",
-    packages=[
-        "kandinsky2",
-        "kandinsky2/vqgan",
-        "kandinsky2/model"
-    ],
+    packages=["kandinsky2", "kandinsky2/vqgan", "kandinsky2/model"],
     install_requires=[
         "Pillow",
         "attrs",
@@ -18,13 +14,12 @@ setup(
         "regex",
         "numpy",
         "blobfile",
-        "transformers==4.23.1",
+        "transformers",
         "torchvision",
         "omegaconf",
         "pytorch_lightning",
         "einops",
         "sentencepiece",
-  
     ],
     author="",
 )
